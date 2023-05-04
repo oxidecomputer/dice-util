2ee31a0338761c8fa8ffee0d33fc144877065cf1
Philip Tricca <flihp@twobit.org>
2023-05-04 09:23:27 -0700

    Use 'anyhow' crate instead of manually boxed `Result`s.

--- dice-mfg/src/main.rs
+++ dice-mfg/src/main.rs
@@ -2,8 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use anyhow::Result;
 use clap::{Parser, Subcommand};
-use dice_mfg::Result;
 use dice_mfg_msgs::PlatformId;
 use env_logger::Builder;
 use log::{info, LevelFilter};
