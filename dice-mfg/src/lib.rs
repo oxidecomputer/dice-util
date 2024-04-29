b62e260678b61b704e5566b84cc27e7f26b1d1af
Philip Tricca <flihp@twobit.org>
2024-04-29 15:55:01 -0700

    dice-mfg: `absolute_path` feature is stable in > 1.80 nightly

--- dice-mfg/src/lib.rs
+++ dice-mfg/src/lib.rs
@@ -2,8 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#![feature(absolute_path)]
-
 use anyhow::{Context, Result};
 use const_oid::ObjectIdentifier;
 use dice_mfg_msgs::{
