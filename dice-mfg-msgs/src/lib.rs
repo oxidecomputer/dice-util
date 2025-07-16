119e972046b4d095198b4de2d07ed48b91ea06b3
Philip Tricca <flihp@twobit.org>
2025-07-16 08:31:55 -0700

    dice-mfg-msgs: Include `no_std` attribute when `std` feature is not
    Previously if `test` was defined then `no_std` was not. This makes the
    `std` feature weird when we run `cargo test` because the standard
    library is enabled while our `std` feature is not. This change makes
    this consistent: `cargo test` will be `no_std` while `cargo test
    --feature std` will not.

--- dice-mfg-msgs/src/lib.rs
+++ dice-mfg-msgs/src/lib.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#![cfg_attr(not(any(test, feature = "std")), no_std)]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 #[cfg(feature = "std")]
 use const_oid::db::rfc4519::COMMON_NAME;
