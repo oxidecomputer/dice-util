186771582ff14a98a0860a30518b9af5ad1f1ee5
Philip Tricca <flihp@twobit.org>
2026-08-25 15:07:04 -0700

    Move ed25519 cert verifier to a module

--- verifier/src/lib.rs
+++ verifier/src/lib.rs
@@ -14,12 +14,15 @@ use sha3::{Digest, Sha3_256};
 use std::collections::HashSet;
 use thiserror::Error;
 use x509_cert::{
-    der::{self, Decode, DecodeValue, Encode, Header, SliceReader},
+    der::{self, Decode, DecodeValue, Header, SliceReader},
     Certificate, PkiPath,
 };
 
 mod p384;
 use p384::{P384CertVerifier, P384CertVerifierError};
+
+mod ed25519;
+use ed25519::{Ed25519CertVerifier, Ed25519CertVerifierError};
 
 /// Errors related to the creation of signature verifiers for certs in a
 /// `PkiPath`.
@@ -74,83 +77,6 @@ pub enum CertVerifierError {
 trait CertVerifier {
     /// Verify some property of the `Certificate` provided.
     fn verify(&self, cert: &Certificate) -> Result<(), CertVerifierError>;
-}
-
-/// Errors produced by the `Ed25519CertVerifier`.
-#[derive(Debug, Error)]
-pub enum Ed25519CertVerifierError {
-    #[error("Spki public key type is not Ed25519")]
-    WrongKeyType,
-    #[error("Public key has params but Ed25519 keys have none")]
-    UnexpectedParam,
-    #[error("Malformed public key")]
-    MalformedPublicKey,
-    #[error("Failed to create verifier from bytes: {0}")]
-    VerifyingKeyFromBytes(#[from] ed25519_dalek::SignatureError),
-}
-
-/// Errors produced when verifying ed25519 signatures over `Certificate`s by
-/// the `Ed25519CertVerifier`.
-struct Ed25519CertVerifier {
-    verifying_key: ed25519_dalek::VerifyingKey,
-}
-
-impl TryFrom<&Certificate> for Ed25519CertVerifier {
-    type Error = Ed25519CertVerifierError;
-
-    /// Create a `CertVerifier` from the provided `Certificate`
-    fn try_from(certificate: &Certificate) -> Result<Self, Self::Error> {
-        use ed25519_dalek::VerifyingKey;
-
-        let spki = &certificate.tbs_certificate.subject_public_key_info;
-        if spki.algorithm.oid != ID_ED_25519 {
-            return Err(Self::Error::WrongKeyType);
-        }
-
-        if spki.algorithm.parameters.is_some() {
-            return Err(Self::Error::UnexpectedParam);
-        }
-
-        let key_bytes = spki
-            .subject_public_key
-            .as_bytes()
-            .ok_or_else(|| Self::Error::MalformedPublicKey)?;
-        let verifying_key = VerifyingKey::try_from(key_bytes)?;
-
-        Ok(Self { verifying_key })
-    }
-}
-
-impl CertVerifier for Ed25519CertVerifier {
-    /// Verify the ed25519 signature on the `Certificate` provided
-    fn verify(&self, cert: &Certificate) -> Result<(), CertVerifierError> {
-        use ed25519_dalek::{Signature, Verifier};
-
-        let algorithm = &cert.signature_algorithm;
-        if algorithm.oid != ID_ED_25519 {
-            return Err(CertVerifierError::SignatureType);
-        }
-
-        if algorithm.parameters.is_some() {
-            return Err(CertVerifierError::UnexpectedParams);
-        }
-
-        let signature = cert
-            .signature
-            .as_bytes()
-            .ok_or(CertVerifierError::MalformedSignature)?;
-        let signature = Signature::try_from(signature)
-            .map_err(|e| CertVerifierError::SignatureConversion(Box::new(e)))?;
-
-        let message = cert
-            .tbs_certificate
-            .to_der()
-            .map_err(|e| CertVerifierError::Message(Box::new(e)))?;
-
-        self.verifying_key
-            .verify(&message, &signature)
-            .map_err(|e| CertVerifierError::Signature(Box::new(e)))
-    }
 }
 
 /// Errors produced by the PkiPathSignatureVerifier
